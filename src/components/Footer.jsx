5897c73e3efcc137581775a47a060a876e9c9620
StefanPDavis <snoodle22@hotmail.com>
2024-03-19 09:57:44 -0700

    added code for main, header, and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        <h4>Made with React by Stefan Davis.</h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        <h1>Stefan's Project Portfolio</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
